69f5bb4f64379a12e42cf41af38fae64875ea669
Luke San Antonio <lukesanantonio@gmail.com>
2013-03-27 22:18:20 -0400

    Renamed the `vec` class to `vector`. The file has also been renamed (vec.hpp -> vector.hpp).

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,17 +1,17 @@
 /*!
- * \file vec.hpp
+ * \file vector.hpp
  * \brief Contains the o-so beautiful vector class.
  *
  * To be used for positions, velocities, anything you want!
  */
-#ifndef ULTIMATE_PONG_VEC_HPP
-#define ULTIMATE_PONG_VEC_HPP
+#ifndef ULTIMATE_PONG_VECTOR_HPP
+#define ULTIMATE_PONG_VECTOR_HPP
 namespace pong
 {
   /*!
    * \brief The wonderful, wonderful vector class.
    */
-  struct vec
+  struct vector
   {
     int x = 0; //!< x (position | velocity | etc)
     int y = 0; //!< y (position | velocity | etc)
