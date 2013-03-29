fe95a15d597d03096583d8ccb847b8b0bd818df8
Luke San Antonio <lukesanantonio@gmail.com>
2013-03-28 21:29:12 -0400

    Fully documented the game state ABC (GameState.h).

--- src/GameState.h
+++ src/GameState.h
@@ -1,12 +1,29 @@
+/*!
+ * \file GameState.h
+ * \brief File containing the ABC for all game states.
+ */
 #ifndef ULTIMATE_PONG_GAME_STATE_H
 #define ULTIMATE_PONG_GAME_STATE_H
 #include <SDL/SDL.h>
 namespace pong
 {
+  /*!
+   * \brief ABC which all game states should publicly inherit from.
+   */
   class GameState
   {
   public:
+    /*!
+     * \brief This function updates the internal state of the object.
+     */
     virtual void update() = 0;
+    
+    /*!
+     * \brief This function renders whatever it needs to render based off of
+     * it's internal state set during GameState::update().
+     *
+     * It should not modify the object, nor should it throw exceptions.
+     */
     virtual void render(SDL_Surface* surface) const noexcept = 0;
   };
 };
