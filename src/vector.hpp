8c8c9bb80a78bd5f6eec7b2f06ae1ada2d638010
Luke San Antonio <lukesanantonio@gmail.com>
2013-03-28 21:09:56 -0400

    Fully documented vector.hpp

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,12 +1,30 @@
+/*!
+ * \file vector.hpp
+ * \brief The header of a beautiful thing, a vector class.
+ */
 #ifndef ULTIMATE_PONG_VECTOR_HPP
 #define ULTIMATE_PONG_VECTOR_HPP
 namespace pong
 {
+  /*!
+   * A namespace for all math related pong things.
+   *
+   * That could be collision detection, vectors, velocities, positions, etc.
+   */
   namespace math
   {
+    /*!
+     * \brief A simple vector class.
+     */
     struct vector
     {
+      /*!
+       * \brief X value for anything imaginable.
+       */
       int x = 0;
+      /*!
+       * \brief Y value for anything imaginable.
+       */
       int y = 0;
     };
   };
