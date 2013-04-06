fb46382a77eb25eb8b12f4126f37ae8fac749782
Luke San Antonio <lukesanantonio@gmail.com>
2013-04-05 22:16:43 -0400

    Added the start of the Button class, it should be done soon.

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,10 +1,82 @@
+/*!
+ * \file Button.cpp
+ * \brief Contains definitions for the Button class.
+ */
 #include "Button.h"
 namespace pong
 {
-  Button::Button(int x, int y, const std::string& label) : x_(x), y_(y),
-                                                           label_(label)
-  {}
+  Button::Button(const std::string& text,
+                 math::vector pos,
+                 std::size_t width,
+                 std::size_t height) :
+                 label_(text),
+                 pos_(pos),
+                 width_(width),
+                 height_(height) {}
+  Button::~Button()
+  {
+    if(this->image_)
+    {
+      SDL_FreeSurface(this->image_);
+    }
+  }
 
-  Button::Button() : Button(0, 0, "")
-  {}
+  void Button::render(SDL_Surface* surface) const
+  {
+    //Generate our image if necessary.
+    this->generateImage();
+
+    //Render image!
+    SDL_Rect button_dest;
+    button_dest.x = this->pos_.x;
+    button_dest.y = this->pos_.y;
+    SDL_BlitSurface(this->image_, NULL, surface, &button_dest);
+
+    //Find where to render the label!
+    math::vector label_pos;
+    label_pos.x = (this->width_ / 2) - (this->label_.getSurfaceWidth() / 2);
+    label_pos.y = (this->height_ / 2) - (this->label_.getSurfaceHeight() / 2);
+    this->label_.position(label_pos);
+
+    //Render the label.
+    this->label_.render(surface);
+  }
+
+  void Button::generateImage() const
+  {
+    if(this->image_is_out_of_date_)
+    {
+      //Make sure we don't leak any memory.
+      if(this->image_)
+      {
+        SDL_FreeSurface(this->image_);
+      }
+
+      //Generate the right sized image.
+      this->image_ = SDL_CreateRGBSurface(SDL_SWSURFACE,
+                                          this->width_, this->height_, 8,
+                                          0, 0, 0, 0);
+      if(!this->image_)
+      {
+        throw std::runtime_error("Failed to create 8-bit image with"
+                                 "SDL_CreateRGBSurface in"
+                                 "Button::generateImage()");
+      }
+      //Generate the palette.
+      SDL_Color white;
+
+      white.r = 0xff;
+      white.g = 0xff;
+      white.b = 0xff;
+
+      SDL_SetColors(this->image_, &white, 0, 1);
+
+      //Fill the surface.
+      SDL_FillRect(this->image_, NULL,
+                   SDL_MapRGB(this->image_->format, 0xff, 0xff, 0xff));
+
+      //Don't do this again.
+      this->image_is_out_of_date_ = false;
+    }
+  }
 };
--- src/Button.h
+++ src/Button.h
@@ -1,24 +1,64 @@
+/*!
+ * \file Button.h
+ * \brief Contains declarations for the Button class.
+ */
 #ifndef ULTIMATE_PONG_BUTTON_H
 #define ULTIMATE_PONG_BUTTON_H
 #include <string>
 #include <SDL/SDL.h>
+#include "Label.h"
+#include "vector.hpp"
 namespace pong
 {
   class Button
   {
   public:
-    Button(int x, int y, const std::string& label);
-    Button();
-    ~Button() noexcept = default;
+    explicit Button(const std::string& text = "",
+                    math::vector pos = math::vector(),
+                    std::size_t width = 0,
+                    std::size_t height = 0);
+    ~Button() noexcept;
     Button(const Button&) = delete;
     Button(Button&&) = delete;
     Button& operator=(const Button&) = delete;
     Button& operator=(Button&&) = delete;
+
+    /*!
+     * \brief Renders everything onto the surface so that the top left corner
+     * of the button is at Button::pos_
+     */
+    void render(SDL_Surface* surface) const;
+
+    /*!
+     * \brief Generates Button::image_, an all-white surface of width and height
+     * of Button::width_ and Button::height_ respectively.
+     *
+     * This function is here to allow for the client to specify generation when
+     * it's a good time, or not, this function is required, since it will be
+     * called when necessary.
+     *
+     * \post Button::image_is_out_of_date_ == false
+     * \post Button::image_->w == Button::width_
+     * \post Button::image_->h == Button::height_
+     *
+     * \throws std::runtime_error if SDL fails to create the surface for any
+     * reason.
+     */
+    void generateImage() const;
   private:
-    SDL_Surface* image_;
-    int x_;
-    int y_;
-    std::string label_;
+    mutable Label label_;
+    math::vector pos_;
+    std::size_t width_;
+    std::size_t height_;
+
+    mutable SDL_Surface* image_ = nullptr;
+    /*!
+     * \brief Whether or not the all-white image used as the button itself needs
+     * to be regenerated.
+     *
+     * \sa Button::image_
+     */
+    mutable bool image_is_out_of_date_ = true;
   };
 };
 #endif
