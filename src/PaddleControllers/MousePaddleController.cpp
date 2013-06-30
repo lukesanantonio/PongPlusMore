b9dffb8a26bf3538757a0690bbe798a74be0d55b
Luke San Antonio <lukesanantonio@gmail.com>
2013-06-29 20:49:47 -0400

    Commented MousePaddleController::update and made the mouse position of the center of the paddle, not the left side.

--- src/PaddleControllers/MousePaddleController.cpp
+++ src/PaddleControllers/MousePaddleController.cpp
@@ -5,14 +5,22 @@ namespace pong
 {
   void MousePaddleController::update(Paddle* paddle)
   {
+    //Get the game instance.
     Game* game = Game::getInstance();
-    math::vector pos = {game->events.mousePosition().x,
-                        paddle->position().y};
+
+    //Position calculation.
+    math::vector pos = {
+         game->events.mousePosition().x - static_cast<int>(paddle->width()) / 2,
+         paddle->position().y};
+
+    //Bounds constraints.
     if(pos.x < 0) pos.x = 0;
     if(pos.x > game->width() - paddle->width())
     {
       pos.x = game->width() - paddle->width();
     }
+
+    //Update position in the paddle.
     paddle->position(pos);
   }
 }
