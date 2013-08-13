c12a6bb8f51d3b2dfbf096ce6078acb9bbf7e5ce
Luke San Antonio <lukesanantonio@gmail.com>
2013-08-13 18:48:02 -0400

    Licensed PpM under the GPL.
    Every file (except for LICENSE) now has a header describing how it
    belongs to PpM and that it is licensed under the GPL.

--- src/Game.h
+++ src/Game.h
@@ -1,3 +1,22 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /*!
  * \file Game.h
  * \brief All global, game-wide functions and classes are (should be) here.
