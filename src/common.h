ba9803768cf260ef613e2e554be4b9794e4adfab
Luke San Antonio <lukesanantonio@gmail.com>
2013-08-14 02:14:41 -0400

    Fixed a typo in src/common.h.
    The "surface" in pong::runtime_assert_surface was misspelled as
    "surfaec". I fixed that, luckily nothing actually called it!

--- src/common.h
+++ src/common.h
@@ -29,5 +29,5 @@ namespace pong
   [[noreturn]] void crash(const std::string& why);
 
   void runtime_assert_surface(SDL_Surface* surface, int width, int height);
-  void runtime_assert_surfaec(SDL_Surface* surface);
+  void runtime_assert_surface(SDL_Surface* surface);
 }
