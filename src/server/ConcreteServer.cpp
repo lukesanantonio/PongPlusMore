caf865a859ab0f5768d8b177dcca50cce9237e37
Luke San Antonio <lukesanantonio@gmail.com>
2013-10-26 16:48:30 -0400

    Added a Really Simple Server implementation.
    Basically: Paddles move!
    Since server/ConcreteServer.cpp is the first cpp file in the server folder. I
    also added a new target which links to ppmcommon.

---
+++ src/server/ConcreteServer.cpp
@@ -0,0 +1,62 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ConcreteServer.h"
+#include "common/center.hpp"
+namespace pong
+{
+  ConcreteServer::ConcreteServer(uint16_t width, uint16_t height) noexcept :
+                     first_paddle_({0, {0, 0}, 200, 30}, 0),
+                     second_paddle_({0, {0, static_cast<double>(height - 30)},
+                                     200, 30}, 0) {}
+
+  PaddleID ConcreteServer::makePaddle()
+  {
+    //This horrible function returns 1 then 2 then 0.
+    //First client gets the first paddle.
+    //Second client gets the second paddle.
+    //Third+ clients get no paddle.
+    if(id_impl_ == 1) std::get<0>(this->first_paddle_).id = 1;
+    if(id_impl_ == 2) std::get<0>(this->second_paddle_).id = 2;
+    else return 0;
+    return id_impl_++;
+  }
+  void ConcreteServer::setPaddleDestination(PaddleID id,
+                                            decltype(Paddle::pos.x) x) noexcept
+  {
+    //A kind of cool, hidden feature of using two ifs rather than an if-else:
+    //If neither paddle has been claimed by a client, one can make both move
+    //to the same place at the same time! Usefullness: None. Reason: Crap
+    //design with this paddle business.
+    if(std::get<0>(this->first_paddle_).id == id)
+    {
+      std::get<1>(this->first_paddle_) = x;
+    }
+    if(std::get<0>(this->second_paddle_).id == id)
+    {
+      std::get<1>(this->second_paddle_) = x;
+    }
+  }
+  void ConcreteServer::step()
+  {
+    std::get<0>(this->first_paddle_).pos.x = std::get<1>(this->first_paddle_);
+    std::get<0>(this->second_paddle_).pos.x =
+                                             std::get<1>(this->second_paddle_);
+  }
+};
---
+++ src/server/ConcreteServer.h
@@ -0,0 +1,45 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <utility>
+#include "Server.h"
+namespace pong
+{
+  struct ConcreteServer : public Server
+  {
+    ConcreteServer(uint16_t width, uint16_t height) noexcept;
+    virtual ~ConcreteServer() noexcept = default;
+
+    virtual PaddleID makePaddle();
+
+    virtual void setPaddleDestination(PaddleID,
+                                      decltype(Paddle::pos.x) x) noexcept;
+    virtual void step();
+  private:
+    using paddle_x_type = decltype(Paddle::pos.x);
+    std::pair<Paddle, paddle_x_type> first_paddle_;
+    std::pair<Paddle, paddle_x_type> second_paddle_;
+
+    /*!
+     * \brief Used to implement makePaddle.
+     */
+    PaddleID id_impl_ = 1;
+  };
+}
