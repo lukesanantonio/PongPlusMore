24a1df4ca14bed62113f8742289e7765ce0782e6
Luke San Antonio <lukesanantonio@gmail.com>
2013-12-07 22:22:52 -0500

    Added two pong::math::vector tests.
    One for the vector equality operator, one for the rotation operation.

---
+++ tests/VectorTests.cpp
@@ -0,0 +1,42 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "common/vector.h"
+#include "gtest/gtest.h"
+#include <cstdint>
+
+TEST(VectorTest, Equality)
+{
+  using pong::math::vector;
+
+  vector<uint16_t> vecui(5, 5);
+  EXPECT_EQ(vecui, vecui);
+
+  vector<double> vecd(5.5, 5.5);
+  EXPECT_EQ(vecd, vecd);
+}
+TEST(VectorTest, Rotation)
+{
+  namespace pm = pong::math;
+
+  auto calc = pm::rotate(pm::vector<int16_t>(1,0), 3.14159);
+  pm::vector<int16_t> expected(-1, 0);
+
+  EXPECT_EQ(expected, calc);
+}
