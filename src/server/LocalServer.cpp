80f1b490bdfe199afad7024cb98f9dcb80d3ee89
Luke San Antonio <lukesanantonio@gmail.com>
2014-01-14 00:30:57 -0500

    There is now a Local Server implementation.
    It is non standard. Stay tuned.

---
+++ src/server/LocalServer.cpp
@@ -0,0 +1,48 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "LocalServer.h"
+namespace pong
+{
+  PaddleID LocalServer::connect()
+  {
+    // We've done a loop, no more ids.
+    if(++id_counter_ == 0x00) throw NoMoreClientsAvailable();
+    this->world_.paddles.emplace_back(id_counter_);
+    return id_counter_;
+  }
+  void LocalServer::setPaddle(PaddleID id, const Paddle& paddle)
+  {
+    auto iter = findPaddleByID(this->world_, id);
+    if(iter == this->world_.paddles.end())
+    {
+      throw InvalidPaddleID();
+    }
+    *iter = paddle;
+  }
+  Paddle LocalServer::getPaddle(PaddleID id)
+  {
+    auto iter = findPaddleByID(this->world_, id);
+    if(iter == this->world_.paddles.end())
+    {
+      throw InvalidPaddleID();
+    }
+    return *iter;
+  }
+}
