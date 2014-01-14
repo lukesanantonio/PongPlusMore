5ac80fb50098dc083452f9d7961d23bbb35a9285
Luke San Antonio <lukesanantonio@gmail.com>
2014-01-14 00:04:24 -0500

    There is now a mechanism to store the various things one might find on the screen.

---
+++ src/server/World.h
@@ -0,0 +1,27 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+namespace pong
+{
+  struct World
+  {
+    std::vector<Paddle> paddles;
+  };
+}
