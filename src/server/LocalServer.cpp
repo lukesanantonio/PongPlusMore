28ab5ccf97061fb55abfa21c6c5717c960a50777
Luke San Antonio <lukesanantonio@gmail.com>
2014-08-26 12:06:02 -0400

    Things no longer get snapped or otherwise pushed into the wall.

--- src/server/LocalServer.cpp
+++ src/server/LocalServer.cpp
@@ -124,6 +124,11 @@ namespace pong
     o1.volume.pos += outside_snap(o1.volume,
                                   cs & ~o1.physics_options.constraints,
                                   o2.volume);
+
+    Volume bounds = {{0,0}, 1000, 1000};
+    VolumeSides wall_sides = extending_sides(o1.volume, bounds);
+    o1.volume.pos += inside_snap(o1.volume, wall_sides, bounds);
+
     cs = flip(cs);
     o2.volume.pos += outside_snap(o2.volume,
                                   cs & ~o2.physics_options.constraints,
