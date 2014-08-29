432db4faad40a3388d7e6d6448c10eb326f78c33
Luke San Antonio <lukesanantonio@gmail.com>
2014-08-29 11:58:53 -0400

    No need for the common/ prefix in an include by volume.h.

--- src/common/volume.h
+++ src/common/volume.h
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
-#include "common/vector.h"
+#include "vector.h"
 #include <unordered_map>
 #include <array>
 namespace pong
