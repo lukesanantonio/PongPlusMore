b716f988f46639e66cb301560781dc78c3daf219
Luke San Antonio <lukesanantonio@gmail.com>
2014-10-05 18:01:25 -0400

    Implemented the exit request in the engine + A basic plugin that uses that request.

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -17,12 +17,97 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <uv.h>
+#include "SDL.h"
 #include "json/json.h"
+
+#include "core/plugin/plugins.h"
+#include "core/io/Logger.h"
+
+std::unique_ptr<pong::Plugin> spawn_plugin(Json::Value const& json) noexcept
+{
+  std::vector<char*> args;
+
+  for(Json::Value const& array_element : json["plugin"])
+  {
+    args.push_back((char*) array_element.asCString());
+  }
+  args.push_back(NULL);
+
+  pong::ipc::Spawn_Options opts;
+  opts.args = &args[0];
+  opts.cwd = NULL;
+  return pong::make_json_plugin<pong::Child_Process>(opts);
+}
+
+struct State
+{
+  bool running = true;
+  SDL_Window* window = nullptr;
+  SDL_Renderer* renderer = nullptr;
+};
+
+struct Request_Visitor : public boost::static_visitor<>
+{
+  Request_Visitor(State& state) noexcept : state_(state) {}
+  template <class Req_Type>
+  void operator()(Req_Type const& req) const noexcept {}
+
+  void operator()(pong::Exit_Req const& req) const noexcept
+  {
+    state_.running = false;
+  }
+private:
+  State& state_;
+};
 
 int main(int argc, char** argv)
 {
   if(argc < 2)
   {
+    std::cerr << "usage: " << argv[0] << " <game-decl>" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  Json::Reader read(Json::Features::strictMode());
+  std::ifstream file(argv[1]);
+
+  Json::Value json;
+  read.parse(file, json);
+
+  using pong::Logger; using pong::Logger_Stream; using pong::Severity;
+  Logger log(Logger_Stream::Err);
+
+  std::string cwd = json.get("cwd", "").asString();
+  if(cwd.size())
+  {
+    log.log(Severity::Info, "Changing directory: '" + cwd + "'");
+    uv_chdir(json.get("cwd", "").asString().c_str());
+  }
+
+  // Spawn the main plugin.
+  if(!json.isMember("plugin"))
+  {
+    log.log(Severity::Error, "No mod specified in game decl spec");
+    return EXIT_FAILURE;
+  }
+
+  log.log(Severity::Info, "Spawning mod: " + json["plugin"][0].asString());
+  std::unique_ptr<pong::Plugin> plugin = spawn_plugin(json);
+
+  State state;
+  Request_Visitor req_visit(state);
+
+  while(state.running)
+  {
+    pong::Request req;
+    while(plugin->poll_request(req))
+    {
+      boost::apply_visitor(req_visit, req);
+    }
   }
 
   return 0;
