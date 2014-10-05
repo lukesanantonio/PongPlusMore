309efa7b0c4dc7b63875128b43f14681ca6c719c
Luke San Antonio <lukesanantonio@gmail.com>
2014-10-05 14:47:35 -0400

    Updated server/plugins.* under core/plugin/plugins.*.

---
+++ src/core/plugin/plugins.cpp
@@ -0,0 +1,63 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "plugins.h"
+#include "../common/utility.h"
+#include "../common/pif/helper.h"
+namespace pong
+{
+  Json_Plugin::Json_Plugin(std::unique_ptr<External_IO> io) noexcept
+                           : io_(std::move(io))
+  {
+    io_->set_read_callback([this](const std::vector<char>& buf)
+    {
+      bufs_.push(buf);
+    });
+  }
+  bool Json_Plugin::poll_request(Request& req) noexcept
+  {
+    io_->step();
+    if(!bufs_.size()) return false;
+
+    Json::Value val;
+
+    Json::Reader read(Json::Features::strictMode());
+
+    using std::begin; using std::end;
+    // While the queued buffer is failing to compile.
+    while(!read.parse(std::string(begin(bufs_.front()), end(bufs_.front())),
+                      val))
+    {
+      // Ignore it and continue on.
+      bufs_.pop();
+      if(!bufs_.size()) return false;
+    }
+
+    bufs_.pop();
+    req = FORMATTER_TYPE(Request)::parse(val);
+    return true;
+  }
+
+  void Json_Plugin::post_response(Response const& res) noexcept
+  {
+    Json::FastWriter w;
+    std::string s = w.write(FORMATTER_TYPE(Response)::dump(res));
+    io_->write(vec_from_string(s));
+  }
+}
---
+++ src/core/plugin/plugins.h
@@ -0,0 +1,60 @@
+/*
+ * PpM - Pong Plus More - A pong clone full of surprises written with C++11.
+ * Copyright (C) 2013  Luke San Antonio
+ *
+ * You can contact me (Luke San Antonio) at lukesanantonio@gmail.com!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include <memory>
+#include <queue>
+#include <vector>
+#include "json/json.h"
+#include "req.h"
+#include "response.h"
+#include "../io/external_io.h"
+namespace pong
+{
+  struct Server_Plugin
+  {
+    virtual bool poll_request(Request& req) noexcept = 0;
+    virtual void post_response(Response const& res) noexcept = 0;
+  };
+
+  struct Json_Plugin : public Server_Plugin
+  {
+    Json_Plugin(std::unique_ptr<External_IO> io) noexcept;
+    ~Json_Plugin() noexcept = default;
+
+    Json_Plugin(Json_Plugin&&) noexcept = default;
+    Json_Plugin(Json_Plugin const&) noexcept = delete;
+
+    Json_Plugin& operator=(Json_Plugin&&) noexcept = default;
+    Json_Plugin& operator=(Json_Plugin const&) noexcept = delete;
+
+    bool poll_request(Request& req) noexcept override;
+    void post_response(Response const& req) noexcept override;
+  private:
+    std::unique_ptr<External_IO> io_;
+    std::queue<std::vector<char> > bufs_;
+  };
+
+  template <class IO_Type, class... Args>
+  std::unique_ptr<Json_Plugin> make_json_plugin(Args&&... args) noexcept
+  {
+    auto io = std::make_unique<IO_Type>(std::forward<Args>(args)...);
+    return std::make_unique<Json_Plugin>(std::move(io));
+  }
+}
