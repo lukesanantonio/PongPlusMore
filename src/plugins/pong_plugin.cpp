0eb2e4824a0a95170f7029f5bb776a2c51dd46cd
Luke San Antonio <lukesanantonio@gmail.com>
2014-10-28 20:48:35 -0400

    Added a basic plugin that starts the game engine and stops it after 2 seconds.

--- src/plugins/pong_plugin.cpp
+++ src/plugins/pong_plugin.cpp
@@ -20,8 +20,40 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+
+#include "core/plugin/req.h"
+#include "core/common/vector.h"
+#include "core/common/pif/helper.h"
+
+void send_request(pong::Request const& req)
+{
+  Json::FastWriter write;
+  std::string str = write.write(FORMATTER_TYPE(pong::Request)::dump(req));
+  std::cout << str << std::flush;
+}
+
 int main(int argc, char** argv)
 {
+  // Sleep a bit before starting.
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // Start the engine with 1000x1000 window.
+  pong::Request req;
+  req.method = "Core.Start";
+
+  req.params = Json::Value(Json::arrayValue);
+
+  namespace math = pong::math;
+  const math::vector<int> bounds = {1000, 1000};
+  req.params.value().append(FORMATTER_TYPE(math::vector<int>)::dump(bounds));
+  req.params.value().append("Pong Plus More");
+
+  send_request(req);
+
+  // Wait for 2 seconds before exiting.
   std::this_thread::sleep_for(std::chrono::seconds(2));
-  std::cout << "{\"id\":\"eh\", \"method\":\"Core.Log\", \"params\":[\"info\", \"Hello!\"]}" << std::endl;
+
+  req.method = "Core.Exit";
+  req.params = boost::none_t{};
+  send_request(req);
 }
