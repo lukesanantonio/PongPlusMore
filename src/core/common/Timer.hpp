b013caad68639783e514a4c6206dd1295b9c56f9
Luke San Antonio <lukesanantonio@gmail.com>
2014-11-02 19:13:28 -0500

    Timer::hasBeen is now Timer::has_been to be consistent with other class interfaces.

--- src/core/common/Timer.hpp
+++ src/core/common/Timer.hpp
@@ -21,39 +21,39 @@
 #include <chrono>
 namespace pong
 {
-  template <class ClockType = std::chrono::high_resolution_clock>
+  template <class Clock_Type = std::chrono::high_resolution_clock>
   class Timer
   {
   public:
     template <class DurationType>
-    bool hasBeen(DurationType duration) const;
+    bool has_been(DurationType duration) const;
 
     template <class DurationType>
-    DurationType hasBeen() const;
+    DurationType has_been() const;
 
     void reset();
   private:
-    std::chrono::time_point<ClockType> time_before_ = ClockType::now();
+    std::chrono::time_point<Clock_Type> time_before_ = Clock_Type::now();
   };
 
-  template <class ClockType>
-  template <class DurationType>
-  bool Timer<ClockType>::hasBeen(DurationType duration) const
+  template <class Clock_Type>
+  template <class Duration_Type>
+  bool Timer<Clock_Type>::has_been(Duration_Type duration) const
   {
-    return ClockType::now() - this->time_before_ >= duration;
+    return Clock_Type::now() - this->time_before_ >= duration;
   }
 
-  template <class ClockType>
-  template <class DurationType>
-  DurationType Timer<ClockType>::hasBeen() const
+  template <class Clock_Type>
+  template <class Duration_Type>
+  Duration_Type Timer<Clock_Type>::has_been() const
   {
     using std::chrono::duration_cast;
-    return duration_cast<DurationType>(ClockType::now() - this->time_before_);
+    return duration_cast<Duration_Type>(Clock_Type::now()-this->time_before_);
   }
 
-  template <class ClockType>
-  void Timer<ClockType>::reset()
+  template <class Clock_Type>
+  void Timer<Clock_Type>::reset()
   {
-    this->time_before_ = ClockType::now();
+    this->time_before_ = Clock_Type::now();
   }
 }
